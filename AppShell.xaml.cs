ee9918f492c4ad296c87530f30ae0be5f82b811a
Mitsy Paz <pmitzi2003@gmail.com>
2026-06-28 05:41:00 -0500

    feat: registrar rutas de navegación en AppShell para las páginas principales

--- AppShell.xaml.cs
+++ AppShell.xaml.cs
@@ -1,10 +1,23 @@
-﻿namespace TravelSecure.Mobile
+﻿using TravelSecure.Mobile.Features.Auth;
+using TravelSecure.Mobile.Features.Incidents;
+using TravelSecure.Mobile.Features.Alerts;
+using TravelSecure.Mobile.Features.Profile;
+
+namespace TravelSecure.Mobile;
+
+public partial class AppShell : Shell
 {
-    public partial class AppShell : Shell
+    public AppShell()
     {
-        public AppShell()
-        {
-            InitializeComponent();
-        }
+        InitializeComponent();
+
+        // Rutas adicionales accesibles por navegación push
+        Routing.RegisterRoute(nameof(RegisterPage), typeof(RegisterPage));
+
+        Routing.RegisterRoute(nameof(ReportePage), typeof(ReportePage));
+
+        Routing.RegisterRoute(nameof(AlertsPage), typeof(AlertsPage));
+
+        Routing.RegisterRoute(nameof(ProfilePage), typeof(ProfilePage));
     }
 }
